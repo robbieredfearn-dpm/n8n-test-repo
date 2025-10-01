11805bf9fba45a20b0fd406e6c6b0b9de835c7a8
Robbie Redfearn <robbie.redfearn@produktmacher.com>
2025-10-01 15:38:24 +0200

    playing with variables

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
+    let test_string = String::from("This is a test string");
+
     println!("Hello, world!");
     println!("And hellooooo n8n test!");
     println!("I wonder if this will work?");
+    println!("{}", test_string);
 }
